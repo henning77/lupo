9500e00c485f47a7da60d8bf425d254981a7d00f
Henning Boeger <henning.boeger@gmail.com>
2013-11-24 15:55:11 +0100

    Implemented simple stats collector logging in csv format

--- src/lupo/lupo.go
+++ src/lupo/lupo.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"encoding/hex"
 	"flag"
+	"log"
 	"fmt"
 	"io"
 	"net"
@@ -20,101 +20,103 @@ var to string
 // If true, use SSL/TLS connections
 var ssl bool
 
+// Interval to collect (ms)
+var interval int
+
 // Count the connections to make them easily identifiable
 var nextConnId = make(chan int)
 
 // Timestamp format for logging
-const stamp = "15:04:05.000"
+const stampf = "02.01.2006 15:04:05.000"
 
-// Special Writer which writes head + nicely formatted binary / textual chunks + tail
-type transferLog struct {
-	head string
-	tail string
+var events = make(chan event, 1000)
+
+type eventKind byte
+const (
+	_                 = iota
+	connect eventKind = iota
+	disconnect
+	send
+	receive
+)
+type event struct {
+	kind eventKind
+	data uint32
 }
 
-// Write nicely formatted binary / textual chunk
-func (l *transferLog) Write(p []byte) (n int, err error) {
-	logPrint(l.head)
+type stats struct {
+	stamp time.Time
+	connCount uint32
+	connOpened uint32
+	connClosed uint32
+	sent uint32
+	received uint32
+}
 
-	binChunk := false
-	chunkStart := 0
-	printableCount := 0
-
-	for i, b := range p {
-		if isPrintable(b) {
-			printableCount++
-
-			// char chunks have a minimum length
-			if binChunk && printableCount >= 5 {
-				// Write the previous binary chunk
-				chunkEnd := i - printableCount + 1
-				writeHexChunk(p[chunkStart:chunkEnd])
-
-				binChunk = false
-				chunkStart = chunkEnd
-			}
-		} else {
-			printableCount = 0
-
-			if !binChunk {
-				// Write the previous char chunk
-				os.Stdout.Write(p[chunkStart:i])
-
-				binChunk = true
-				chunkStart = i
-			}
+func statsCollector() {
+	stats := stats{stamp: time.Now()}
+	ticker := time.Tick(time.Duration(interval) * time.Millisecond)
+	for {
+		select {
+			case e := <-events:
+				switch e.kind {
+					case connect:
+						stats.connCount++
+						stats.connOpened++
+					case disconnect:
+						stats.connCount--
+						stats.connClosed++
+					case send:
+						stats.sent += e.data
+					case receive:
+						stats.received += e.data					
+				}
+			case <-ticker:
+				printStats(&stats)
+				resetStats(&stats)
 		}
 	}
+}
 
-	// Final chunk
-	if binChunk {
-		writeHexChunk(p[chunkStart:])
-	} else {
-		os.Stdout.Write(p[chunkStart:])
-	}
+func resetStats(s *stats) {
+	// Note: connCount is not reset, as we want to track the absolute value
+	s.stamp = time.Now()
+	s.connOpened = 0
+	s.connClosed = 0
+	s.sent = 0
+	s.received = 0
+}
 
-	os.Stdout.WriteString(l.tail)
+func printStatHeader() {
+	os.Stdout.WriteString("Date;ConnCount;ConnOpened;ConnClosed;Sent;Received;TotalTransferred\n")
+}
+
+func printStats(s *stats) {
+	os.Stdout.WriteString(s.stamp.Format(stampf))
+	fmt.Printf(";%d;%d;%d;%d;%d;%d\n", s.connCount, s.connOpened, s.connClosed, s.sent, s.received, s.sent + s.received)
+}
+
+type statsWriter struct {
+	direction eventKind
+}
+
+func (s *statsWriter) Write(p []byte) (n int, err error) {
+	events <- event{kind:s.direction, data:uint32(len(p))}
 	return len(p), nil
 }
 
-func writeHexChunk(p []byte) {
-	dumper := hex.Dumper(os.Stdout)
-	dumper.Write(p)
-	dumper.Close()
-}
-
-func isPrintable(b byte) bool {
-	return b == 0x0d || b == 0x0a || (b >= 0x20 && b <= 0x7e)
-}
-
-// Log with optimized timestamp
-func logPrintf(s string, a ...interface{}) {
-	os.Stdout.WriteString(time.Now().Format(stamp))
-	os.Stdout.WriteString(" ")
-	os.Stdout.WriteString(fmt.Sprintf(s, a...))
-	os.Stdout.WriteString("\n")
-}
-
-// Log with optimized timestamp
-func logPrint(s string) {
-	os.Stdout.WriteString(time.Now().Format(stamp))
-	os.Stdout.WriteString(" ")
-	os.Stdout.WriteString(s)
-	os.Stdout.WriteString("\n")
-}
-
 // Copy to dst and to logger
-func copyWithLog(dst io.Writer, src io.Reader, head string, tail string) {
-	logger := &transferLog{head: head, tail: tail}
+func copyWithLog(dst io.Writer, src io.Reader, direction eventKind, done chan<- bool) {
+	logger := &statsWriter{direction:direction}
 	multi := io.MultiWriter(dst, logger)
 	if _, err := io.Copy(multi, src); err != nil {
-		logPrintf("Closed connection: %v", err)
+		//logPrintf("Closed connection: %v", err)
 	}
+	done <- true	
 }
 
 func handleConnection(src net.Conn) {
-	connId := <-nextConnId
-	logPrint(fmt.Sprintf("New connection: %v (from %v)", connId, src.RemoteAddr()))
+	events <- event{kind:connect}
 
 	var dst net.Conn
 	var err error
@@ -125,28 +127,26 @@ func handleConnection(src net.Conn) {
 	}
 	
 	if err != nil {
-		logPrintf("Error connecting to dest: %v", err)
-		panic(err)
+		log.Panicf("Error connecting to dest: %v", err)
 	}
+
+	done := make(chan bool)
 
 	// Copy & log in both directions
-	go copyWithLog(dst, src, fmt.Sprintf("->%v", connId), "\n")
-	go copyWithLog(src, dst, fmt.Sprintf("<-%v", connId), "\n")
-}
+	go copyWithLog(dst, src, send, done)
+	go copyWithLog(src, dst, receive, done)
 
-func genConnectionIds() {
-	i := 1
-	for {
-		nextConnId <- i
-		i++
-	}
+	// Wait for connection to close
+	<-done
+	events <- event{kind:disconnect}
+	// Wait for the other stream to close as well
+	<-done
 }
 
 func tlsServerConfig() *tls.Config {
 	cert, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
 	if err != nil {
-		logPrintf("Could not read server certificate (cert.pem, key.pem): %v", err)
-		os.Exit(1)
+		log.Panicf("Could not read server certificate (cert.pem, key.pem): %v", err)
 	}
 	return &tls.Config{Certificates: []tls.Certificate{cert}}
 }
@@ -160,6 +160,7 @@ func init() {
 	flag.StringVar(&from, "from", "", "Source host/port to listen to. Example: ':8081'")
 	flag.StringVar(&to, "to", "", "Destination host/port to forward to. Example: 'localhost:8080'")
 	flag.BoolVar(&ssl, "ssl", false, "If true, expect and provide SSL/TLS connections. Needs cert.pem + key.pem in the same directory")
+	flag.IntVar(&interval, "interval", 1000, "Interval in which to collect (ms)")
 }
 
 func main() {
@@ -169,7 +170,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	logPrintf("Listening to [%v], forwarding to [%v]", from, to)
+	printStatHeader()
+	go statsCollector()
 
 	var ln net.Listener
 	var err error
@@ -180,17 +182,13 @@ func main() {
 	}
 	
 	if err != nil {
-		logPrintf("Could not open port: %v", err)
-		os.Exit(1)
+		log.Panicf("Could not open port: %v", err)
 	}
-
-	go genConnectionIds()
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			logPrintf("Error accepting: %v", err)
-			continue
+			log.Panicf("Error accepting: %v", err)
 		}
 		go handleConnection(conn)
 	}
