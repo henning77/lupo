aef9f0f12427892baa67ae2d3b67777ab9f32aec
Henning Boeger <henning.boeger@gmail.com>
2013-11-30 13:19:41 +0100

    Improved usage output

--- src/lupo/lupo.go
+++ src/lupo/lupo.go
@@ -5,20 +5,29 @@ import (
 	"lupo/printer"
 	"lupo/server"
 	"os"
+	"fmt"
 )
 
-
-// Usage examples
-// lupo -from :8080 -to myserver:80
-// lupo -from :8443 -to myserver:443 -ssl
-// lupo -style full -from :8080 -to myserver:80
+const version = "v0.2.0"
 
 var style string
 
+func appHeader() {
+	fmt.Printf("lupo %v\n\n", version)
+}
+
+func usageExamples() {
+	fmt.Println("Examples:")
+	fmt.Println("  lupo -from :8080 -to myserver:80")
+	fmt.Println("  lupo -from :8443 -to myserver:443 -ssl")
+	fmt.Println("  lupo -style full -from :8080 -to myserver:80")	
+	fmt.Println("  lupo -style stats -from :8080 -to myserver:80")
+}
+
 func init() {
-	flag.StringVar(&style, "style", "short", "Style of output. 'short' prints one line per transfer. 'full' prints everything. 'stats' prints a csv with stats.")
-	flag.StringVar(&server.From, "from", server.From, "Source host/port to listen to. Example: ':8081'")
-	flag.StringVar(&server.To, "to", server.To, "Destination host/port to forward to. Example: 'localhost:8080'")
+	flag.StringVar(&style, "style", "short", "Style of output. 'short' prints one line per transfer. 'full' prints everything. 'stats' prints a csv with stats")
+	flag.StringVar(&server.From, "from", server.From, "Source host/port to listen to")
+	flag.StringVar(&server.To, "to", server.To, "Destination host/port to forward to")
 	flag.BoolVar(&server.Ssl, "ssl", server.Ssl, "If true, expect and provide SSL/TLS connections. Needs cert.pem + key.pem in the same directory")
 	//flag.IntVar(&printer.MaxPayloadCharsToPrint, "trunc", printer.MaxPayloadCharsToPrint, "Truncate content (number of chars to print)")
 	//flag.BoolVar(&printer.Headers, "headers", printer.Headers, "Print HTTP headers")
@@ -28,7 +37,10 @@ func init() {
 func main() {
 	flag.Parse()
 	if len(server.From) == 0 || len(server.To) == 0 {
+		appHeader()
 		flag.Usage()
+		fmt.Println()
+		usageExamples()
 		os.Exit(1)
 	}
 
